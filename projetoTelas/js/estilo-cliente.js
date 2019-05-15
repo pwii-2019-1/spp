6f49e44a1f5bb5a89b1d166d8a4929ce06e0f933
[AnaPaulaMatos] <[ateodorodematos22@hotmail.com]>
2019-05-14 22:05:44 -0300

    Telas de Cadastros SPP

---
+++ projetoTelas/js/estilo-cliente.js
@@ -0,0 +1,129 @@
+  function limpa_formulario() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('nome').value=("");
+            document.getElementById('cpf').value=("");
+            document.getElementById('rg').value=("");
+            document.getElementById('sexo').value=("");
+            document.getElementById('dataNascimento').value=("");
+            document.getElementById('tel').value=("");
+            document.getElementById('email').value=("");
+            document.getElementById('logradouro').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('estado').value=("");
+            document.getElementById('cep').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('nome').value=(conteudo.nome);
+            document.getElementById('cpf').value=(conteudo.cpf);
+            document.getElementById('rg').value=(conteudo.rg);
+            document.getElementById('sexo').value=(conteudo.sexo);
+            document.getElementById('dataNascimento').value=(conteudo.dataNascimento);
+            document.getElementById('tel').value=(conteudo.tel);
+            document.getElementById('email').value=(conteudo.email);
+            document.getElementById('logradouro').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.cidade);
+            document.getElementById('estado').value=(conteudo.estado);
+            document.getElementById('cep').value=(conteudo.cep);
+        } //end if.
+        else {
+            //CPF não Encontrado.
+            limpa_formulario_cpf();
+            alert("CPF não encontrado.");
+            document.getElementById('cpf').value=("");
+        }
+    }
+
+    function pesquisacpf(valor) {
+
+        //Nova variável "cpf" somente com dígitos.
+        var cpf = valor.replace(/\D/g, '');
+
+        //Verifica se campo cpf possui valor informado.
+        if (cpf !== "") {
+
+            //Expressão regular para validar o cpf.
+            var validacpf = /^[0-12]{11}$/;
+
+            //Valida o formato do cpf.
+            if(validacpf.test(cpf)) {
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = '//viacpf.com.br/ws/'+ cpf + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulario_cpf();
+                alert("Formato de CPF inválido.");
+            }
+        } //end if.
+        else {
+            //cpf sem valor, limpa formulário.
+            limpa_formulario_cpf();
+        }
+    }
+
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i);
+
+  if (texto.substring(0,1) != saida) {
+            documento.value += texto.substring(0,1);
+  }
+
+}
+
+function idade (){
+    var data=document.getElementById("dtnasc").value;
+    var dia=data.substr(0, 2);
+    var mes=data.substr(3, 2);
+    var ano=data.substr(6, 4);
+    var d = new Date();
+    var ano_atual = d.getFullYear(),
+        mes_atual = d.getMonth() + 1,
+        dia_atual = d.getDate();
+
+        ano=+ano,
+        mes=+mes,
+        dia=+dia;
+
+    var idade=ano_atual-ano;
+
+    if (mes_atual < mes || mes_atual == mes_aniversario && dia_atual < dia) {
+        idade--;
+    }
+return idade;
+}
+
+function exibe(i) {
+	document.getElementById(i).readOnly= true;
+}
+
+function desabilita(i) {
+  document.getElementById(i).disabled = true;
+    }
+function habilita(i) {
+  document.getElementById(i).disabled = false;
+    }
+
+function showhide() {
+       var div = document.getElementById("newpost");
+       if(idade()>=18) {
+    div.style.display = "none";
+}
+else if(idade()<18) {
+    div.style.display = "inline";
+  }
+}
---
+++ projetoTelas/js/estilo-produto.js
@@ -0,0 +1,89 @@
+function limpa_formulario() {
+          //Limpa valores do formulário.
+          document.getElementById('descricaoProd').value=("");
+          document.getElementById('numeracao').value=("");
+          document.getElementById('genero').value=("");
+          document.getElementById('cor').value=("");
+          document.getElementById('marca').value=("");
+          document.getElementById('valorUnitario').value=("");
+          document.getElementById('saldoProduto').value=("");
+  }
+
+  function meu_callback(conteudo) {
+      if (!("erro" in conteudo)) {
+          //Atualiza os campos com os valores.
+          document.getElementById('descricaoProd').value=(conteudo.descricaoProd);
+          document.getElementById('numeracao').value=(conteudo.numeracao);
+          document.getElementById('genero').value=(conteudo.genero);
+          document.getElementById('cor').value=(conteudo.cor);
+          document.getElementById('marca').value=(conteudo.marca);
+          document.getElementById('valorUnitario').value=(conteudo.valorUnitario);
+          document.getElementById('saldoProduto').value=(conteudo.saldoProduto);
+      } //end if.
+      else {
+          //Descrição não Encontrada.
+          limpa_formulario_descricaoProd();
+          alert("Descrição não encontrada.");
+          document.getElementById('descricaoProd').value=("");
+      }
+  }
+
+  function pesquisadescricaoProd(valor) {
+
+      //Nova variável "cpf" somente com dígitos.
+      var descricaoProd = valor.replace(/\D/g, '');
+
+      //Verifica se campo descricaoProd possui valor informado.
+      if (descricaoProd !== "") {
+
+          //Expressão regular para validar o descricaoProd.
+          var validadescricaoProd = /^[0-30]{29}$/;
+
+          //Valida o formato do descricaoProd.
+          if(validadescricaoProd.test(descricaoProd)) {
+
+              //Cria um elemento javascript.
+              var script = document.createElement('script');
+
+              //Sincroniza com o callback.
+              script.src = '//viadescricaoProd.com.br/ws/'+ descricaoProd + '/json/?callback=meu_callback';
+
+              //Insere script no documento e carrega o conteúdo.
+              document.body.appendChild(script);
+
+          } //end if.
+          else {
+              //descricaoProd é inválida.
+              limpa_formulario_descricaoProd();
+              alert("Formato de descrição é inválida.");
+          }
+      } //end if.
+      else {
+          //descricaoProd sem valor, limpa formulário.
+          limpa_formulario_descricaoProd();
+      }
+  }
+
+function formatar(mascara, documento){
+    var i = documento.value.length;
+    var saida = mascara.substring(0,1);
+    var texto = mascara.substring(i);
+
+if (texto.substring(0,1) != saida){
+          documento.value += texto.substring(0,1);
+        }
+}
+
+function exibe(i) {
+document.getElementById(i).readOnly= true;
+  }
+
+function desabilita(i) {
+   document.getElementById(i).disabled = true;
+  }
+
+function habilita(i) {
+      document.getElementById(i).disabled = false;
+  }
+
+}
